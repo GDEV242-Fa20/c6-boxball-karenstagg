acb1f6df731bba7d1c5e5971d5f4b4b15d8f7f61
karenstagg <42982417+karenstagg@users.noreply.github.com>
2020-10-07 11:48:56 -0400

    Create boxBounce Method box dimensions
    set box dimensions for boxBounce method and change class header author/version.

--- BallDemo.java
+++ BallDemo.java
@@ -4,8 +4,8 @@ import java.awt.Color;
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
  *
- * @author Michael Kölling and David J. Barnes
- * @version 2016.02.29
+ * @author Karen Stagg
+ * @version October 12, 2020
  */
 
 public class BallDemo   
@@ -50,5 +50,41 @@ public class BallDemo
                 finished = true;
             }
         }
+    }
+    
+    /**
+     * Simulate multiple balls bouncing inside a box.
+     */
+    public void boxBounce()
+    {
+        int ground = 400;   // position of the ground line
+
+        myCanvas.setVisible(true);
+
+        // draw the box
+        myCanvas.setForegroundColor(Color.BLACK);
+        myCanvas.drawLine(20, 10, 580, 10);
+        myCanvas.drawLine(20, 10, 20, 400);
+        myCanvas.drawLine(20, 400, 580, 400);
+        myCanvas.drawLine(580,400, 580, 10);
+
+        // create and show the balls
+        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+        ball.draw();
+        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
+        ball2.draw();
+
+        // make them bounce
+        boolean finished =  false;
+        while (!finished) {
+            myCanvas.wait(50);           // small delay
+            ball.move();
+            ball2.move();
+            // stop once ball has travelled a certain distance on x axis
+            if(ball.getXPosition() >= 560 || ball2.getXPosition() >= 560) {
+                finished = true;
+            }
+        }
+    
     }
 }
